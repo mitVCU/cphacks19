46755e5e55ebb3f6317c0039339a71a02368280c
dbkwolf <33721623+dbkwolf@users.noreply.github.com>
2019-04-06 23:19:04 +0200

    camera is working

--- app/src/main/java/com/example/cphacks19/MainActivity.kt
+++ app/src/main/java/com/example/cphacks19/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cphacks19
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -7,21 +8,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import com.google.firebase.FirebaseApp
 
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import android.widget.Toast
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var dbRef: DatabaseReference
+    //private lateinit var dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        val dbRef = FirebaseDatabase.getInstance().reference
 
+
+       // FirebaseApp.initializeApp(this)
+        //dbRef = FirebaseDatabase.getInstance().reference
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -47,6 +56,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
 
     }
+
+
 
 
 }
