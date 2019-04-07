b9389d5bd758a08ca4b9600b2010787d399e7f70
kjronning <kjronning@gmail.com>
2019-04-07 10:46:05 +0200

    WE ACTUALLY GET A VALUE NOW! HOLY SHIT!!!

--- app/src/main/java/com/example/cphacks19/PlayActivity.kt
+++ app/src/main/java/com/example/cphacks19/PlayActivity.kt
@@ -9,25 +9,25 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 
+import kotlin.concurrent.schedule
 import com.google.android.cameraview.CameraView
 
 import kotlinx.android.synthetic.main.activity_play.*
-import android.widget.Toast
-import android.content.DialogInterface
-import android.support.annotation.StringRes
 import android.os.Environment.DIRECTORY_PICTURES
-import android.R.layout
-import android.app.AlertDialog
-import android.app.Dialog
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.Environment
-import android.os.Handler
-import android.support.v4.app.DialogFragment
+import android.widget.Toast
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStream
+import java.util.*
+import android.support.v4.view.accessibility.AccessibilityEventCompat.setAction
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.net.Uri
+import android.util.AttributeSet
+import android.widget.ImageView
 
 
 class PlayActivity : AppCompatActivity() {
@@ -35,12 +35,18 @@ class PlayActivity : AppCompatActivity() {
 
     private val TAG = "Play activity"
     private lateinit var model : EmotionViewModel
-    private var emotionName = "anger"
+    private var emotionName = "neutral"
+    private var score = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
-
+        model = EmotionViewModel()
+        model.getEmotionLiveData().observe(this, android.arch.lifecycle.Observer { value -> kotlin.run{
+            Log.d(TAG, "observing data change")
+            score += value!!.toFloat()
+            Toast.makeText(this, model.getEmotionLiveData().value!!.toString(),Toast.LENGTH_SHORT).show()
+        }})
         if (checkCameraHardware(this)) {
             init()
         }
@@ -50,8 +56,73 @@ class PlayActivity : AppCompatActivity() {
         if (camera == null) {
             println("camera is null")
         }
+
+        camera.facing = CameraView.FACING_FRONT
+        camera.addCallback(getCameraCallback())
+        camera.start()
+        startTimer()
     }
 
+    private fun getCameraCallback(): CameraView.Callback {
+        return object : CameraView.Callback() {
+
+            override fun onCameraOpened(cameraView: CameraView?) {
+                Log.d(TAG, "onCameraOpened")
+            }
+
+            override fun onCameraClosed(cameraView: CameraView?) {
+                Log.d(TAG, "onCameraClosed")
+            }
+
+            override fun onPictureTaken(cameraView: CameraView, data: ByteArray) {
+                Log.d(TAG, "onPictureTaken " + data.size)
+                cameraView.handler.post {
+                    val file = File(
+                        getExternalFilesDir(DIRECTORY_PICTURES),
+                        "picture.jpg"
+                    )
+                    var os: OutputStream? = null
+                    try {
+                        os = FileOutputStream(file)
+                        os.write(data)
+                        os.close()
+                        var bitmap = BitmapFactory.decodeFile(file.path)
+                        val bOutput: Bitmap
+                        val matrix = Matrix()
+                        matrix.preScale(1.0f, -1.0f)
+                        bOutput = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+
+                        Log.d(TAG,"image height: "+bitmap.height+" ,image width: "+bitmap.width)
+                        FaceRecognizer.detectAndSetEmotionValue(bOutput, emotionName,this@PlayActivity)
+                    } catch (e: IOException) {
+                        Log.w(TAG, "Cannot write to $file", e)
+                    } finally {
+                        if (os != null) {
+                            try {
+                                os.close()
+                            } catch (e: IOException) {
+                                // Ignore
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun newIntent(context: Context, image: Bitmap, file: File): Intent {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.fromFile(file))
+        intent.setDataAndType(Uri.fromFile(file), "image/*")
+        return intent
+    }
+
+    private fun startTimer() {
+        Log.d(TAG, "Scheduling take picture")
+        Timer().schedule(3000){
+            camera.takePicture()
+        }
+    }
 
     private fun checkCameraHardware(context: Context): Boolean {
         return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
@@ -72,50 +143,6 @@ class PlayActivity : AppCompatActivity() {
         camera.stop()
         super.onPause()
     }
-
-
-    private val mCallback = object : CameraView.Callback() {
-
-        override fun onCameraOpened(cameraView: CameraView?) {
-            Log.d(TAG, "onCameraOpened")
-        }
-
-        override fun onCameraClosed(cameraView: CameraView?) {
-            Log.d(TAG, "onCameraClosed")
-        }
-
-        override fun onPictureTaken(cameraView: CameraView, data: ByteArray) {
-            Log.d(TAG, "onPictureTaken " + data.size)
-            cameraView.handler.post {
-                val file = File(
-                    getExternalFilesDir(DIRECTORY_PICTURES),
-                    "picture.jpg"
-                )
-                var os: OutputStream? = null
-                try {
-                    os = FileOutputStream(file)
-                    os.write(data)
-                    os.close()
-                    var bitmap = BitmapFactory.decodeFile("/path/images/image.jpg")
-                    var recognizer = FaceRecognizer(this@PlayActivity)
-                    recognizer.detectAndSetEmotionValue(bitmap,emotionName)
-                } catch (e: IOException) {
-                    Log.w(TAG, "Cannot write to $file", e)
-                } finally {
-                    if (os != null) {
-                        try {
-                            os.close()
-                        } catch (e: IOException) {
-                            // Ignore
-                        }
-
-                    }
-                }
-            }
-        }
-
-    }
-
 }
 
 
